aa8e58df6a4712eb67bb6ec5890bab60c0b40da3
wabyking <810553967@qq.com>
2015-03-31 17:27:36 +0800

    some change

--- src/cn/edu/tju/tianchi/feature/impl/UserFeature.java
+++ src/cn/edu/tju/tianchi/feature/impl/UserFeature.java
@@ -13,17 +13,7 @@ import cn.edu.tju.tianchi.util.UserItemRecord;
 
 public class UserFeature implements IUserFeature{
 
-	
-	public int getUserFeature(List<UserItemRecord> records, String outfile) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		return 0;
-	}
+
 
 	@Override
 	public List<IUserProfile> getUserFeature(List<UserItemRecord> records,List<ItemRecord> itemRecord) {
